5e213f23257116fa8ccacdfb41c79bfca66c2dd3
Aaditya Bhatia <aadityabhatia@gmail.com>
2020-03-20 21:31:22 -0400

    build USB + ISO + PXE + UEFI images by default

--- build-ipxe.sh
+++ build-ipxe.sh
@@ -6,10 +6,12 @@ set -ve
 menu=$(readlink -e menu.ipxe)
 pwd=$(pwd)
 
+BUILD_OUTPUT="bin/ipxe.usb bin/ipxe.iso bin/undionly.kpxe bin-x86_64-efi/ipxe.efi"
+
 cd /usr/local/src/
 test -d ipxe && (cd ipxe && git pull) || git clone https://git.ipxe.org/ipxe.git
 cd ipxe/src/
 sed -i '/DOWNLOAD_PROTO_HTTPS/ s/^[^ \t]*[ \t]/#define /' config/general.h
 sed -i '/IMAGE_TRUST_CMD/ s/^[^ \t]*[ \t]/#define /' config/general.h
-make bin/undionly.kpxe bin-x86_64-efi/ipxe.efi EMBED=$menu
-mv -v bin/undionly.kpxe bin-x86_64-efi/ipxe.efi $pwd
+make $BUILD_OUTPUT EMBED=$menu
+mv -v $BUILD_OUTPUT $pwd
