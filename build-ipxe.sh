25e87b63d2a04fa1b4c4de5d8245baebcc7020eb
Aaditya Bhatia <aadityabhatia@gmail.com>
2021-01-10 14:50:37 -0500

    updated iPXE git URL (github)

--- build-ipxe.sh
+++ build-ipxe.sh
@@ -9,7 +9,7 @@ pwd=$(pwd)
 BUILD_OUTPUT="bin/ipxe.usb bin/ipxe.iso bin/undionly.kpxe bin-x86_64-efi/ipxe.efi"
 
 cd /usr/local/src/
-test -d ipxe && (cd ipxe && git pull) || git clone https://git.ipxe.org/ipxe.git
+test -d ipxe && (cd ipxe && git pull) || git clone https://github.com/ipxe/ipxe.git
 cd ipxe/src/
 sed -i '/DOWNLOAD_PROTO_HTTPS/ s/^[^ \t]*[ \t]/#define /' config/general.h
 sed -i '/IMAGE_TRUST_CMD/ s/^[^ \t]*[ \t]/#define /' config/general.h
